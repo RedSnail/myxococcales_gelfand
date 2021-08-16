c9c8fff1001d7ba6ddb620e1a9dcdd82a6cde4a8
RedSnail <oleg.demianchenko@gmail.com>
2021-08-16 05:25:08 +0300

    skeleton for prepare step launch

---
+++ launch_panacota.sh
@@ -0,0 +1,9 @@
+# source /home/demianchenko/anaconda3/bin/activate
+# conda activate myxococcales
+
+while IFS="" read -r fam || [ -n "$fam" ]
+do
+	mkdir ${fam}_pan &> /dev/null
+	taxids=($(gawk -v family=${fam} -F'\t' '{if ($7 == family) {print $5}}' myxococcales_table.tsv | sort -u > /dev/stdout))
+	echo ${taxids[@]}
+done < families_for_analysis.list
