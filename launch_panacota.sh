10ec8beda7da40a1a3ce6160c01a13b3a7a16d28
RedSnail <oleg.demianchenko@gmail.com>
2021-09-03 12:31:55 +0300

    i dont know what i changed

--- launch_panacota.sh
+++ launch_panacota.sh
@@ -4,6 +4,6 @@
 while IFS="" read -r fam || [ -n "$fam" ]
 do
 	mkdir ${fam}_pan &> /dev/null
-	taxids=($(gawk -v family=${fam} -F'\t' '{if ($7 == family) {print $5}}' myxococcales_table.tsv | sort -u > /dev/stdout))
-	echo ${taxids[@]}
+	taxids=$(gawk -v family=${fam} -F'\t' '{if ($7 == family) {print $5}}' myxococcales_table.tsv | sort -u > /dev/stdout)
+	echo $taxids
 done < families_for_analysis.list
